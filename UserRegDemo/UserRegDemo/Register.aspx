070be0ac30b08f9325615ca20439106fa6258ee6
Hola01 <i@mezzp.com>
2019-09-20 14:45:50 +0800

    注册例子

---
+++ UserRegDemo/UserRegDemo/Register.aspx
@@ -0,0 +1,126 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="UserRegDemo.Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 50%;
+        }
+        .auto-style2 {
+            height: 20px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table cellpadding="2" cellspacing="0" class="auto-style1">
+            <tr>
+                <td>【用户注册】</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>用户名:</td>
+                <td>
+                    <asp:TextBox ID="txtUname" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:RequiredFieldValidator ID="reqtxtUname" runat="server" ControlToValidate="txtUname" ErrorMessage="必须填写用户名">*</asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>密码:</td>
+                <td>
+                    <asp:TextBox ID="txtPwd" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:RequiredFieldValidator ID="reqtxtPwd" runat="server" ControlToValidate="txtPwd" ErrorMessage="必须填写密码">*</asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>确认密码:</td>
+                <td>
+                    <asp:TextBox ID="txtRePwd" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:RequiredFieldValidator ID="reqtxtPwd0" runat="server" ControlToValidate="txtRePwd" ErrorMessage="必须填写确认密码">*</asp:RequiredFieldValidator>
+                    <asp:CompareValidator ID="comPwd" runat="server" ControlToCompare="txtPwd" ControlToValidate="txtRePwd" Display="Dynamic" ErrorMessage="确认密码与密码不同">*</asp:CompareValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>姓名:</td>
+                <td>
+                    <asp:TextBox ID="txtName" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:RequiredFieldValidator ID="reqtxtName" runat="server" ControlToValidate="txtName" ErrorMessage="必须填写姓名">*</asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>性别:</td>
+                <td>
+                    <asp:RadioButtonList ID="radioSex" runat="server" Width="140px">
+                        <asp:ListItem>男</asp:ListItem>
+                        <asp:ListItem>女</asp:ListItem>
+                    </asp:RadioButtonList>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>年龄:</td>
+                <td>
+                    <asp:TextBox ID="txtAge" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:RangeValidator ID="reqtxtAge" runat="server" ControlToValidate="txtAge" ErrorMessage="必须填写有效的年龄" MaximumValue="100" MinimumValue="1" Type="Integer">*（1~100）</asp:RangeValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td>
+                    <asp:TextBox ID="txtEmail" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:RegularExpressionValidator ID="regtxtEmail" runat="server" ControlToValidate="txtEmail" ErrorMessage="必须填写有效的E-mail" ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*">例:i@mezzp.com</asp:RegularExpressionValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">QQ:</td>
+                <td class="auto-style2">
+                    <asp:TextBox ID="txtQQ" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+                <td class="auto-style2">
+                    <asp:RegularExpressionValidator ID="regtxtQQ" runat="server" ControlToValidate="txtQQ" ErrorMessage="必须填写有效的QQ" ValidationExpression="\d{5,12}">例:12345</asp:RegularExpressionValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <asp:Button ID="btnOK" runat="server" Text="注册" />
+                    <input id="Reset1" type="reset" value="重置" /></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <asp:ValidationSummary ID="valError" runat="server" ShowMessageBox="True" ShowSummary="False" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ UserRegDemo/UserRegDemo/Register.aspx.designer.cs
@@ -0,0 +1,186 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能会导致不正确的行为，并且如果
+//     重新生成代码，这些更改将会丢失。 
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace UserRegDemo {
+    
+    
+    public partial class Register {
+        
+        /// <summary>
+        /// form1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// txtUname 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUname;
+        
+        /// <summary>
+        /// reqtxtUname 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqtxtUname;
+        
+        /// <summary>
+        /// txtPwd 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPwd;
+        
+        /// <summary>
+        /// reqtxtPwd 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqtxtPwd;
+        
+        /// <summary>
+        /// txtRePwd 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRePwd;
+        
+        /// <summary>
+        /// reqtxtPwd0 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqtxtPwd0;
+        
+        /// <summary>
+        /// comPwd 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator comPwd;
+        
+        /// <summary>
+        /// txtName 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+        
+        /// <summary>
+        /// reqtxtName 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqtxtName;
+        
+        /// <summary>
+        /// radioSex 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList radioSex;
+        
+        /// <summary>
+        /// txtAge 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAge;
+        
+        /// <summary>
+        /// reqtxtAge 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator reqtxtAge;
+        
+        /// <summary>
+        /// txtEmail 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        
+        /// <summary>
+        /// regtxtEmail 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator regtxtEmail;
+        
+        /// <summary>
+        /// txtQQ 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtQQ;
+        
+        /// <summary>
+        /// regtxtQQ 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator regtxtQQ;
+        
+        /// <summary>
+        /// btnOK 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnOK;
+        
+        /// <summary>
+        /// valError 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary valError;
+    }
+}
